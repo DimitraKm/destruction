f4ea53d24ac2b04e189727d56b0c4516eb725944
DimitraKm <d.kontaki13@gmail.com>
2023-05-07 12:04:31 +0300

    Add project files.

---
+++ destruction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace destruction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            person s1 = new person(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine("Hello, my name is " + s1.name);
+            Console.WriteLine("Hello, my name is " + s1.name2);
+            Console.WriteLine("Hello, my name is " + s1.name3);
+
+        }
+    }
+}
---
+++ destruction/names.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace destruction
+{
+    internal class person
+    {
+        public string name;
+        public string name2;
+        public string name3;
+
+        public person(string name, string name2, string name3)
+        {
+            this.name = name;
+            this.name2 = name2;
+            this.name3 = name3;
+        }
+    }
+}
